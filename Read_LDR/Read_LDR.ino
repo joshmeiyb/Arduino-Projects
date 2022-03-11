f81b61dbdcfbb6a69056b2ec0f38017683d57968
joshmeiyb <mei27@wisc.edu>
2022-03-11 15:51:53 -0600

    add LDR reading task

---
+++ Read_LDR/Read_LDR.ino
@@ -0,0 +1,28 @@
+#define LDR_PIN A1
+#define LED_PIN 4
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LDR_PIN, INPUT);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int value = analogRead(A1);
+
+  Serial.print("Analog Value : ");
+  Serial.println(value);
+
+  if(value < 500){
+    digitalWrite(LED_PIN, HIGH);
+  }
+  else{
+    digitalWrite(LED_PIN, LOW);
+  }
+
+  delay(250);
+
+
+}
